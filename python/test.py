b3e4d3c16c8736bcb30b546c53b0d72aed38b4c3
Tiago Ildefonso <tmdgi@protonmail.com>
2024-02-04 13:56:02 +0000

    added generated int lens

--- python/test.py
+++ python/test.py
@@ -1,1 +1,11 @@
-print('Hello, world!')
+from math import *
+
+for i in range(144):
+    int_exp  = int(exp(i));       int_len_exp  = len(str(int_exp))
+    int_fact = int(factorial(i)); int_len_fact = len(str(int_fact))
+    int_pow  = int(pow(i, i));    int_len_pow = len(str(int_pow))
+    #print(f"[{i}]")
+    #print(f"{int_exp}\n{int_fact}\n{int_pow}")
+    #print(f"lens: {int_len_exp} << {int_len_fact} << {int_len_pow}")
+    print(f"diff_lens: {int_len_fact - int_len_exp} << {int_len_pow - int_len_fact}")
+    
